2f7909f5e0ea5117890ed42becdbd509b031d6fd
Chris Chua <someone42@gmail.com>
2012-06-14 07:03:32 +1000

    adding support for P2SH transactions

--- baseconv.h
+++ baseconv.h
@@ -11,9 +11,13 @@
 #include "common.h"
 
 /** Address version to use when converting 160 bit hashes to human-readable
-  * Bitcoin addresses. This should be 0x00 for the main network or 0x6f for
-  * testnet. */
-#define ADDRESSVERSION		0x00
+  * non-P2SH ("pubkey hash") Bitcoin addresses. This should be 0x00 for the
+  * main network or 0x6f for testnet. */
+#define ADDRESS_VERSION_PUBKEY		0x00
+/** Address version to use when converting 160 bit hashes to human-readable
+  * P2SH ("script hash") Bitcoin addresses. This should be 0xc4 for the main
+  * network or 0x6f for testnet. */
+#define ADDRESS_VERSION_P2SH		0x05
 
 /** Required size of a buffer which stores the text of a transaction output
   * amount. This includes the terminating null. */
@@ -23,7 +27,7 @@
 #define TEXT_ADDRESS_LENGTH	36
 
 extern void amountToText(char *out, uint8_t *in);
-extern void hashToAddr(char *out, uint8_t *in);
+extern void hashToAddr(char *out, uint8_t *in, uint8_t address_version);
 
 #endif // #ifndef BASECONV_H_INCLUDED
 
