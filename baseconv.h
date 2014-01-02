6a7d065d35bf896f9e5132946a4a32743deb0888
Chris Chua <someone42@gmail.com>
2014-01-03 05:25:11 +1100

    adding TESTNET define for easier switching to testnet mode

--- baseconv.h
+++ baseconv.h
@@ -13,11 +13,19 @@
 /** Address version to use when converting 160 bit hashes to human-readable
   * non-P2SH ("pubkey hash") Bitcoin addresses. This should be 0x00 for the
   * main network or 0x6f for testnet. */
+#ifndef TESTNET
 #define ADDRESS_VERSION_PUBKEY		0x00
+#else // #ifndef TESTNET
+#define ADDRESS_VERSION_PUBKEY		0x6f
+#endif // #ifndef TESTNET
 /** Address version to use when converting 160 bit hashes to human-readable
-  * P2SH ("script hash") Bitcoin addresses. This should be 0xc4 for the main
-  * network or 0x6f for testnet. */
+  * P2SH ("script hash") Bitcoin addresses. This should be 0x05 for the main
+  * network or 0xc4 for testnet. */
+#ifndef TESTNET
 #define ADDRESS_VERSION_P2SH		0x05
+#else // #ifndef TESTNET
+#define ADDRESS_VERSION_PUBKEY		0xc4
+#endif // #ifndef TESTNET
 
 /** Required size of a buffer which stores the text of a transaction output
   * amount. This includes the terminating null. */
